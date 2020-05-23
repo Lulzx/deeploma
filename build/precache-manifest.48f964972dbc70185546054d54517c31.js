793236812df629fee966fdbe9cbcc728202a0e6b
greenjew <grihan45@gmail.com>
2020-05-23 21:12:31 +0300

    first build

---
+++ build/precache-manifest.48f964972dbc70185546054d54517c31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5763c73a8e0c4d84374b0c25abe99151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bc49e6db67c7075898a",
+    "url": "/static/css/2.4b998d96.chunk.css"
+  },
+  {
+    "revision": "2d6e6233f590eeb0e796",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "0bc49e6db67c7075898a",
+    "url": "/static/js/2.fdcb8d09.chunk.js"
+  },
+  {
+    "revision": "bb83b8736d1b3c303e61b31e3954dbf9",
+    "url": "/static/js/2.fdcb8d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6e6233f590eeb0e796",
+    "url": "/static/js/main.8c2d08c8.chunk.js"
+  },
+  {
+    "revision": "4ba6581ebc9fae078681",
+    "url": "/static/js/runtime-main.f193c1b5.js"
+  }
+]);
